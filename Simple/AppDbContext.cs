4ee8f74944796e557e9ecbebf362000f87794c78
Hanish <hanish.jnet@outlook.com>
2019-04-21 16:32:08 +0530

    First COmmmit

---
+++ Simple/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Simple
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
